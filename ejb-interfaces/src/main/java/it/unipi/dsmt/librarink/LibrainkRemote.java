1195aad0c578a83b5022afba14c1274ab7cf7ea5
Tumminelli96 <56186785+Tumminelli96@users.noreply.github.com>
2022-09-07 00:40:32 +0200

    Added deleted filtered search for users

--- ejb-interfaces/src/main/java/it/unipi/dsmt/librarink/LibrainkRemote.java
+++ ejb-interfaces/src/main/java/it/unipi/dsmt/librarink/LibrainkRemote.java
@@ -4,8 +4,8 @@ import javax.ejb.Remote;
 import java.util.List;
 @Remote
 public interface LibrainkRemote {
-    public List<libraink_usersDTO> listUsers();
-    public List<libraink_booksDTO> listBooks();
+    public List<libraink_usersDTO> listUsers(libraink_usersDTO userFilter);
+    public List<libraink_booksDTO> listBooks(libraink_booksDTO booksFilter);
     public List<libraink_history_loanDTO> listHistoryLoan();
     public List<libraink_wishlistDTO> listWishlist();
     public List<libraink_history_reservationDTO> listHistoryReservation();
