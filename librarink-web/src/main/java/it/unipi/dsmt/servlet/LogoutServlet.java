4b7a65c5f013c781152de9515359793aacd4f241
Federico Cristofani <84182920+federico-cristofani@users.noreply.github.com>
2022-09-25 21:01:45 +0200

    Logout

---
+++ librarink-web/src/main/java/it/unipi/dsmt/servlet/LogoutServlet.java
@@ -0,0 +1,17 @@
+package it.unipi.dsmt.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "LogoutServlet", value = "/logout")
+public class LogoutServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            request.logout();
+            response.sendRedirect(request.getContextPath() + "/pages/jsp/login.jsp");
+    }
+}
