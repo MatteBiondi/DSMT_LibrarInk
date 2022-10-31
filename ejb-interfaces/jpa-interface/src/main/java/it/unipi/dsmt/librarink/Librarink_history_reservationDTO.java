94e2e5cc5e358ee3ef4fda484cf1e653df8d9684
Tumminelli96 <56186785+Tumminelli96@users.noreply.github.com>
2022-10-31 16:03:59 +0100

    id_copy deleted in the history_reservation java class

--- ejb-interfaces/jpa-interface/src/main/java/it/unipi/dsmt/librarink/Librarink_history_reservationDTO.java
+++ ejb-interfaces/jpa-interface/src/main/java/it/unipi/dsmt/librarink/Librarink_history_reservationDTO.java
@@ -5,7 +5,6 @@ import java.sql.Date;
 
 public class Librarink_history_reservationDTO implements Serializable {
     String user_email;
-    String id_copy;
     String isbn;
     Date start_date;
     Date end_date;
@@ -17,14 +16,6 @@ public class Librarink_history_reservationDTO implements Serializable {
 
     public void setUser_email(String user_email) {
         this.user_email = user_email;
-    }
-
-    public String getId_copy() {
-        return id_copy;
-    }
-
-    public void setId_copy(String id_copy) {
-        this.id_copy = id_copy;
     }
 
     public String getIsbn() {
@@ -63,7 +54,6 @@ public class Librarink_history_reservationDTO implements Serializable {
     public String toString() {
         return "libraink_history_reservationDTO{" +
                 "user_email='" + user_email + '\'' +
-                ", id_copy='" + id_copy + '\'' +
                 ", isbn='" + isbn + '\'' +
                 ", start_date=" + start_date +
                 ", end_date=" + end_date +
