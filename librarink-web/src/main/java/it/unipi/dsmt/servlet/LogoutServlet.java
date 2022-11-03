dd9329b50bfb3b47b811bc2d227cb003b1b42f9d
Matteo Biondi <matteobiondi1197@gmail.com>
2022-11-03 09:45:02 +0100

    Logout implementation

--- librarink-web/src/main/java/it/unipi/dsmt/servlet/LogoutServlet.java
+++ librarink-web/src/main/java/it/unipi/dsmt/servlet/LogoutServlet.java
@@ -5,13 +5,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LogoutServlet", value = "/logout")
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            request.logout();
-            response.sendRedirect(request.getContextPath() + "/pages/jsp/login.jsp");
+        //invalidate the session if exists
+        HttpSession session = request.getSession(false);
+        session.invalidate();
+        request.logout();
+        response.sendRedirect(request.getContextPath() + "/pages/jsp/login.jsp");
     }
 }
